e643b016b96837b7e2e8943fe41ea1b65671502f
qdd-hub <qkrdmsddn123@gmail.com>
2025-12-16 17:00:39 +0900

    Update picomain.py

--- picomain.py
+++ picomain.py
@@ -1,4 +1,4 @@
-import machine
+mport machine
 import sys
 import time
 import struct
@@ -10,16 +10,9 @@ mic1 = machine.ADC(26)
 mic2 = machine.ADC(27)
 
 while True:
-    # 1. 두 마이크의 값을 읽습니다.
-    val1 = mic1.read_u16()  # 0 ~ 65535
-    val2 = mic2.read_u16()  # 0 ~ 65535
-    
-    # 2. 데이터를 4바이트 덩어리로 묶습니다. (Mic1 2바이트 + Mic2 2바이트)
-    # '>HH'는 Big-Endian 방식으로 Unsigned Short(2byte) 두 개라는 뜻
+    # 최대한 빨리 읽고 보냄
+    val1 = mic1.read_u16()
+    val2 = mic2.read_u16()
     packet = struct.pack('>HH', val1, val2)
-    
-    # 3. USB로 전송
     sys.stdout.buffer.write(packet)
-    
-    # 너무 빠르면 그래프가 못 따라갈 수 있으니 미세한 딜레이 (필요시 조절)
-    time.sleep_ms(2)
+    # sleep 없음!
